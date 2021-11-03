ef47a575bbf993b63db0bf1b717301d54aabaff6
ayesha <ayeshamasood159@hotmail.com>
2021-11-04 00:35:52 +0500

    commit updates
    todo changes

---
+++ version4/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const item_Collection_1 = require("./item Collection");
+console.clear();
+let col1 = new item_Collection_1.ItemCollection();
+col1.addTodo("Purchase Mango");
+col1.addTodo("Eat Biryani");
+col1.addTodo("Tikka");
+col1.printDetails();
---
+++ version5/dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const item_Collection_1 = require("./item Collection");
+console.clear();
+let col1 = new item_Collection_1.ItemCollection();
+col1.addTodo("Purchase Mango");
+col1.addTodo("Eat Biryani");
+col1.addTodo("Tikka");
+col1.taskDone(2);
+col1.taskDone(1);
+col1.printDetails();
